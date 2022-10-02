88e4ee3f5eff739e7fb90985a64f4984a7141dbd
Stone Comstock <comstockstone@yahoo.com>
2022-10-02 16:30:42 -0700

    working on rendering sond card components

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -6,6 +6,7 @@ const Discover = () => {
   const genreTitle = 'Pop';
   return (
     <div className='flex flex-col'>
+      {/* select genre section */}
       <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
         <h2 className='font-bold text-3xl text-white text-left'>
           Discover {genreTitle}
@@ -16,9 +17,21 @@ const Discover = () => {
           className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
         >
           {genres.map((genre) => (
-            <option>{genre.title}</option>
+            <option key={genre.value} value={genre.value}>
+              {genre.title}
+            </option>
           ))}
         </select>
+      </div>
+      {/* wrapper div for all songs */}
+      <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+        {[1,2,3,4,5,6,7,8,9,10].map((song, i) => (
+            <SongCard 
+            key={song.key}
+            song={song}
+            i={i}
+            />
+        ))}
       </div>
     </div>
   );
