3545e5bdef083d7e082bb9c47d256ade491a4739
Stone Comstock <comstockstone@yahoo.com>
2022-10-02 17:38:40 -0700

    working on playpause button component

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,9 +1,12 @@
+import { useDispatch, useSelector } from 'react-redux';
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
+  const dispatch = useDispatch();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
 
   const genreTitle = 'Pop';
 
@@ -32,7 +35,14 @@ const Discover = () => {
       <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
         {/* map over all songs and render songcard for each one */}
         {data?.map((song, i) => (
-          <SongCard key={song.key} song={song} i={i} />
+          <SongCard
+            key={song.key}
+            song={song}
+            i={i}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data}
+          />
         ))}
       </div>
     </div>
